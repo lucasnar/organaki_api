a65da96e18a60d0dd44149faea37e8783280e995
ramon-passos <ramonpassos2011@gmail.com>
2023-05-11 21:37:14 -0300

    fix db host

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :organaki_api, OrganakiApi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "organaki_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
