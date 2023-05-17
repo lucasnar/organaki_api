58b9e685a33fe0e07f2fa840d25c69dc69ed579e
Ramon Passos <81271031+ramon-passos@users.noreply.github.com>
2023-05-16 22:04:17 -0300

    add api service on docker-compose file (#2)
    * add api service on docker-compose file
    
    * fix db host
    
    * fix development port
    
    * add server profile and update readme

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :organaki_api, OrganakiApi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "organaki_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -19,7 +19,7 @@ config :organaki_api, OrganakiApi.Repo,
 config :organaki_api, OrganakiApiWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
